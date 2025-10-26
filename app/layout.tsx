e15de7db0155b4d64782b36c38a5b282279eb720
Manav Gurnani <manavgurnani21@gmail.com>
2025-10-26 14:27:43 -0700

    Update title to Todo List Supreme - Branch B

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Todo List Supreme - Branch B',
+  description: 'Ultimate productivity todo application',
   generator: 'v0.app',
 }
 
